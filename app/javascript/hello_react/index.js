65e78efdee5982965d9740211058546b3b408b87
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2022-01-20 17:27:17 +0900

    shim環境で動作しなかったので
    DOMContentLoaded が実行されない

--- app/javascript/hello_react/index.js
+++ app/javascript/hello_react/index.js
@@ -2,12 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Hello from "./hello";
 
-document.addEventListener('DOMContentLoaded', () => {
-  const container = document.querySelector("#hello");
-  if (container) {
-    return ReactDOM.render(
-      React.createElement(Hello, { toWhat: container.dataset.toWhat }, null),
-      container
-    );
-  }
-});
+const container = document.querySelector("#hello");
+if (container) {
+  ReactDOM.render(
+    React.createElement(Hello, { toWhat: container.dataset.toWhat }, null),
+    container
+  );
+}
